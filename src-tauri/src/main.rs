20f0bc749b615c511e2bc711011993068fd8b7a0
rongang <xuronggang1997@163.com>
2026-01-18 20:46:51 +0800

    fix：更新关羽域名、标题

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -60,12 +60,12 @@ fn open_new_window(app: tauri::AppHandle, url: Option<String>) -> Result<(), Str
             tauri::WindowUrl::External(parsed)
         }
         _ => tauri::WindowUrl::External(
-            url::Url::parse("https://zhanma.rfqwt.cn/").map_err(|e| e.to_string())?,
+            url::Url::parse("https://guany.tmntiib.cn/").map_err(|e| e.to_string())?,
         ),
     };
 
     tauri::WindowBuilder::new(&app, label, window_url)
-        .title("百里玄策")
+        .title("关羽")
         .inner_size(775.0, 800.0)
         .center()
         .build()
